3a52aff0acea027cbe11dad0a5aa2e5bae37ea76
Simon Friedrich <simon.friedrich@iem.thm.de>
2017-08-10 16:49:42 +0200

    Matlab GUI Erstentwurf

---
+++ Matlab Anwendung/maingui.m
@@ -0,0 +1,138 @@
+function varargout = maingui(varargin)
+% MAINGUI MATLAB code for maingui.fig
+%      MAINGUI, by itself, creates a new MAINGUI or raises the existing
+%      singleton*.
+%
+%      H = MAINGUI returns the handle to a new MAINGUI or the handle to
+%      the existing singleton*.
+%
+%      MAINGUI('CALLBACK',hObject,eventData,handles,...) calls the local
+%      function named CALLBACK in MAINGUI.M with the given input arguments.
+%
+%      MAINGUI('Property','Value',...) creates a new MAINGUI or raises the
+%      existing singleton*.  Starting from the left, property value pairs are
+%      applied to the GUI before maingui_OpeningFcn gets called.  An
+%      unrecognized property name or invalid value makes property application
+%      stop.  All inputs are passed to maingui_OpeningFcn via varargin.
+%
+%      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
+%      instance to run (singleton)".
+%
+% See also: GUIDE, GUIDATA, GUIHANDLES
+
+% Edit the above text to modify the response to help maingui
+
+% Last Modified by GUIDE v2.5 10-Aug-2017 16:43:53
+
+% Begin initialization code - DO NOT EDIT
+gui_Singleton = 1;
+gui_State = struct('gui_Name',       mfilename, ...
+                   'gui_Singleton',  gui_Singleton, ...
+                   'gui_OpeningFcn', @maingui_OpeningFcn, ...
+                   'gui_OutputFcn',  @maingui_OutputFcn, ...
+                   'gui_LayoutFcn',  [] , ...
+                   'gui_Callback',   []);
+if nargin && ischar(varargin{1})
+    gui_State.gui_Callback = str2func(varargin{1});
+end
+
+if nargout
+    [varargout{1:nargout}] = gui_mainfcn(gui_State, varargin{:});
+else
+    gui_mainfcn(gui_State, varargin{:});
+end
+% End initialization code - DO NOT EDIT
+
+
+% --- Executes just before maingui is made visible.
+function maingui_OpeningFcn(hObject, eventdata, handles, varargin)
+% This function has no output args, see OutputFcn.
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+% varargin   command line arguments to maingui (see VARARGIN)
+
+% Choose default command line output for maingui
+handles.output = hObject;
+
+% Update handles structure
+guidata(hObject, handles);
+
+% UIWAIT makes maingui wait for user response (see UIRESUME)
+% uiwait(handles.figure1);
+
+
+% --- Outputs from this function are returned to the command line.
+function varargout = maingui_OutputFcn(hObject, eventdata, handles) 
+% varargout  cell array for returning output args (see VARARGOUT);
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Get default command line output from handles structure
+varargout{1} = handles.output;
+
+
+% --- Executes on button press in btnGrayscale.
+function btnGrayscale_Callback(hObject, eventdata, handles)
+% hObject    handle to btnGrayscale (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+
+% --- Executes on button press in btnSend1.
+function btnSend1_Callback(hObject, eventdata, handles)
+% hObject    handle to btnSend1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+
+% --- Executes on button press in btnSend2.
+function btnSend2_Callback(hObject, eventdata, handles)
+% hObject    handle to btnSend2 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+
+% --- Executes on button press in btnResult.
+function btnResult_Callback(hObject, eventdata, handles)
+% hObject    handle to btnResult (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+
+
+function txtFile_Callback(hObject, eventdata, handles)
+% hObject    handle to txtFile (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of txtFile as text
+%        str2double(get(hObject,'String')) returns contents of txtFile as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function txtFile_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to txtFile (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+% --- Executes on button press in btnSearch.
+function btnSearch_Callback(hObject, eventdata, handles)
+% hObject    handle to btnSearch (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+
+% --- Executes on button press in btnOpen.
+function btnOpen_Callback(hObject, eventdata, handles)
+% hObject    handle to btnOpen (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
