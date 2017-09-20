94b750d3282f6fe2ebc52e60b9a28e3ebf7d766b
Simon Friedrich <simon.friedrich@iem.thm.de>
2017-09-20 17:23:34 +0200

    Added few comments to source-code of matlab app

--- Matlab Anwendung/ImgCore.m
+++ Matlab Anwendung/ImgCore.m
@@ -8,20 +8,24 @@ classdef ImgCore < handle
    
    methods
        function openImage(self)
+           % Shows windows explorer window to select and open a file
            [filename, dirname] = uigetfile({'*.jpg';'*.bmp';'*.png'},'Image Selector');
            self.fileName = [ dirname filename ];
            self.rawImage = imread(self.fileName);
        end
        
        function displayRawImage(self, target)
+           % displays the rwa image of last opened file
            imagesc(self.rawImage, 'Parent', target);
        end
        
        function displayImage(self, img, target)
+           % used to display any given image in a specified axis
            imagesc(img, 'Parent', target);
        end
        
        function gs = grayscale(self)
+           % convert the last opened picture into an grayscale image
            lum = self.rawImage(:, :, 1) * .2126 + self.rawImage(:, :, 2) * .7152 + self.rawImage(:, :, 3) * .0722;
             
            gs = cat(3, lum, lum, lum);
@@ -30,15 +34,6 @@ classdef ImgCore < handle
        
        function relErr = relError(self, img1, img2)
            % Returns relative Error in percent
-%            re = 0;
-%            for i = 1:255
-%                if img1(i)>=img2(i)
-%                    re = re + (img1(i) - img2(i));
-%                else
-%                    re = re + (img2(i) - img1(i));
-%                end
-%            end
-%            relErr = re * 100 / sum(img1);
             re = abs(img1 - img2);
             relErr = (sum(re)*100) / sum(img1);
        end
